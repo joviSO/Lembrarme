27e1b858a5991f3f7e00886fc318030343154250
João <levelupdojoao@hotmail.com>
2025-01-14 11:02:40 -0300

    corrigindo layout de cards da tela inicial e correção na atualização de status de items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: %i[ show edit update destroy ]
+  before_action :set_item, only: %i[ show edit update destroy toggle_checked]
 
   # GET /items or /items.json
   def index
